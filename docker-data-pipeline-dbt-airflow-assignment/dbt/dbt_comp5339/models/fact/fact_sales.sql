db3c36dbe4dc36882ca42a7f919c70c7d7575aa3
Mateo Vallejo <mval0359@uni.sydney.edu.au>
2024-10-01 01:01:44 +1000

    Base code

---
+++ docker-data-pipeline-dbt-airflow-assignment/dbt/dbt_comp5339/models/fact/fact_sales.sql
@@ -0,0 +1,10 @@
+{{ config(
+    materialized = 'table',
+    unique_key = ['customer_key', 'product_key', 'channel_key', 'bought_date_key', 'geography_key']
+) }}
+
+SELECT
+    customer_key,
+     -- fill code here 
+FROM
+    {{ ref('staging_transactions') }}
