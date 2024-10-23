9f2d0bd6be9a133aa681e9f210a5acef5fbea0ea
mini-baekho <qq923080312@gmail.com>
2024-10-23 22:11:22 +1100

    Update dim_geography.sql

--- docker-data-pipeline-dbt-airflow-assignment/dbt/dbt_comp5339/models/dim/dim_geography.sql
+++ docker-data-pipeline-dbt-airflow-assignment/dbt/dbt_comp5339/models/dim/dim_geography.sql
@@ -3,6 +3,6 @@
       unique_key = 'geographykey'
 ) }}
 
-SELECT id,cityname,countryname,regionname -- fill code here 
+SELECT id as geography_key, city as city_name, countryname as country_name, regionname as region_name -- fill code here 
 FROM
       {{ ref('geography') }}
